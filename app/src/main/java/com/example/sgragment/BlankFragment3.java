ca8c225e8d5080c50a0d3c2ccb226cac651a43a1
ayushsingh-22 <ayushkkumar2205@gmail.com>
2024-03-28 17:33:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sgragment/BlankFragment3.java
@@ -0,0 +1,26 @@
+package com.example.sgragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BlankFragment3 extends Fragment {
+
+    public BlankFragment3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank3, container, false);
+        TextView txtv3 = view.findViewById(R.id.text3);
+        return view;
+    }
+}
